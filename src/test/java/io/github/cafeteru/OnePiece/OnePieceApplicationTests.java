cf3ad106b8f0c3dcd7da5a7b84b1d1db81a71d50
Iván González Mahagamage <cafeteru.dev@gmail.com>
2024-02-10 00:33:30 +0100

    Create project

---
+++ src/test/java/io/github/cafeteru/OnePiece/OnePieceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cafeteru.OnePiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
